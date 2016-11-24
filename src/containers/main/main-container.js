ae677c17339b7ccdb2cf9a11c6a346982630774a
Ahmet Atasoy <menthena@gmail.com>
2016-11-24 09:03:00 +0000

    Feat: Styling components

--- src/containers/main/main-container.js
+++ src/containers/main/main-container.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import CategoryContainer from './category-container';
 import ProductListContainer from './product-list-container';
 import ProductContainer from './product-container';
+import './main-container.css';
 
 const MainContainer = ({ params }) => {
   return (
